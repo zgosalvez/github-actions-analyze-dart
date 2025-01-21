a7401a152dab2392204105d89e38b9c089897e0f
Nebojša Cvetković <nebkat@gmail.com>
2025-01-21 13:50:58 +0000

    Use --no-fatal-warnings

--- dist/index.js
+++ dist/index.js
@@ -27565,7 +27565,7 @@ async function analyze(workingDirectory) {
     }
   };
 
-  const args = ['--format', 'machine'];
+  const args = ['--no-fatal-warnings', '--format', 'machine'];
   args.push('.');
 
   await exec.exec('dart analyze', args, options);
